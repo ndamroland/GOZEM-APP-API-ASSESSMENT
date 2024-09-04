eb3033690903c751c347e8963c02625ec83d0f42
ndamroland <infos.ndam.roland@gmail.com>
2024-09-04 19:04:19 +0100

    Assertion to Validate that the booking no longer exists (should return 404) added

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
 e2e: {
 baseUrl: 'https://restful-booker.herokuapp.com/apidoc/index.html',
 setupNodeEvents(on, config) {
 // implement node event listeners here
+
 },
+
 },
+
+
 });
